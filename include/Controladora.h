5c1d2aeeb3178efdf8add59188a8263a18a5a194
matos-ayrton <ayrtonsilvamatos@gmail.com>
2025-10-01 08:11:07 -0300

    Manutenção para simular de 1 a 5 hidrômetros finalizada

--- include/Controladora.h
+++ include/Controladora.h
@@ -3,20 +3,29 @@
 
 #include "Entrada.h"
 #include "Hidrometro.h"
-#include "Display.h" 
+#include "Display.h"
 #include <string>
-#include <iomanip> 
+#include <atomic> // Necessário para std::atomic
+#include <iomanip>
 
 class Controladora {
 public:
-    Controladora(const std::string& arquivo_config);
-    void executarSimulacao();
+    // NOVO CONSTRUTOR
+    Controladora(int id_simulador, const std::string& arquivo_config);
+    
+    // NOVO EXECUTAR SIMULACAO (para uso com threads)
+    void executarSimulacao(std::atomic<bool>& flag_execucao_global);
     
 private:
     Entrada entrada;
     Hidrometro hidrometro;
-    Display display; 
+    Display display;
     int ultimo_m3_inteiro;
+    int id_simulador; 
+
+    // Adicionado para geração de imagem baseada em tempo
+    int contador_iteracoes; 
+    const int INTERVALO_IMAGEM_SEGUNDOS = 5; // 5 minutos = 300 segundos
 };
 
 #endif
